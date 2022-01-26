005c83f576e7f46328df3b3530d47410fa8106d1
Fabian Kelvans <fkelvans@gmail.com>
2022-01-26 07:01:05 +0300

    Image-Hover-Liquid-Effect

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.wrapper'),
+    intensity: 0.5,
+    image1: "/assets/images/image-3.jpg",
+    image2: "/assets/images/image-5.jpg",
+
+    displacementImage:"/assets/images/image-5.jpg"
+})
